2a4007c76b70c7ade7fc1018bc465a3ca078a54c
IsaacIhoon-python <ihoonisaac@gmail.com>
2026-01-09 18:16:33 +0100

    Added loop and random numbers

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "yes";
+
+        while (playAgain.ToLower() == "yes")
+    {
+
+    // Now i wil generate a magic number from 1 to 100
+    Random randomGenerator = new Random();
+    int magicNumber = randomGenerator.Next(1, 101);
+
+    int guess = -1;
+    int guessCount = 0;
+
+    // Keep looping until the user guess the magic number.
+    while (guess != magicNumber)
+    {
+        Console.Write("What is your guess? ");
+        guess = int.Parse(Console.ReadLine());
+        guessCount++;
+
+        if (guess < magicNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (guess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+        else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+    }
+    // Display number of guesses 
+    Console.WriteLine($"It took you {guessCount} guesses.");
+
+    // Ask user if he wants to play again
+    Console.WriteLine("Do you want to play again? (yes/no) ");
+    playAgain = Console.ReadLine();
+        }
     }
 }
