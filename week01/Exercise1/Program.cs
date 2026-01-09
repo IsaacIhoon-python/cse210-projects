65ac31c5796e2641acd9ea82fa991257a41d6b53
IsaacIhoon-python <ihoonisaac@gmail.com>
2026-01-09 13:48:04 +0100

    fetch user beside the line

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,10 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("What is your first name?");
+        Console.Write("What is your first name?");
         string firstName = Console.ReadLine();
 
-        Console.WriteLine("What is your Last name?");
+        Console.Write("What is your Last name?");
         string lastName = Console.ReadLine();
 
         Console.WriteLine();
