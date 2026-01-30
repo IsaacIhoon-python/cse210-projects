112047ac4400615aa85aa2b868437ab4e4f2f1fc
IsaacIhoon-python <ihoonisaac@gmail.com>
2026-01-30 03:01:07 +0100

    Added the Comment section and a screen short of the program

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,9 +1,42 @@
-using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+
+        Video video1 = new Video("How to Bake Bread", "Isaac", 540);
+        Video video2 = new Video("Understanding C# OOP", "Bright", 620);
+        Video video3 = new Video("Top 10 Hacking Tips", "Fletcher", 480);
+
+        
+        
+        video1.AddComment(new Comment("Stanley", "This was very helpful!"));
+        video1.AddComment(new Comment("John", "Loved the step-by-step explanation."));
+        video1.AddComment(new Comment("Marry", "Can you do a video on how to Package the Bread? "));
+
+
+        video2.AddComment(new Comment("Benjamin", "This is so insightful!"));
+        video2.AddComment(new Comment("Prince", "I Love the detailed explanation."));
+        video2.AddComment(new Comment("Lucky", "Can you also make a video on Python? "));
+
+        video3.AddComment(new Comment("Solomon", "Wao! I never thought this was possible...."));
+        video3.AddComment(new Comment("Francis", "I just want to be sure if its's ethical ."));
+        video3.AddComment(new Comment("Victoria", "Can you do a follow-up video on Penetration testing? "));
+
+        List<Video> videos = new List<Video> { video1, video2, video3};
+
+        foreach (var video in videos)
+        {
+            Console.WriteLine($"Title: {video.Title}");
+            Console.WriteLine($"Author: {video.Author}");
+            Console.WriteLine($"Length: {video.LengthInSeconds} seconds");
+            Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
+            Console.WriteLine("Comments:");
+            video.DisplayComments();
+            Console.WriteLine(new string('_', 40));
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+
     }
 }
