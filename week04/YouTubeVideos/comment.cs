d2d746757e470ede3482c633e1b11158bb4a4011
IsaacIhoon-python <ihoonisaac@gmail.com>
2026-01-30 03:15:10 +0100

    Added Comments for clarification.

--- week04/YouTubeVideos/comment.cs
+++ week04/YouTubeVideos/comment.cs
@@ -1,27 +1,33 @@
 using System;
 using System.Collections.Generic;
 
+// Class representing a Comment
 public class Comment
 {
+    // Properties
     public string CommenterName { get; set; }
     public string Text { get; set;}
 
-
+    // Constructor
     public Comment(string commenterName, string text)
     {
         CommenterName = commenterName;
         Text = text;
     }
 }
-    
+    // Class representing a Video
     public class Video
     {
+        // Properties
         public string Title { get; set;}
         public string Author { get; set;}
         public int LengthInSeconds { get; set;}
-    
 
+    
+        // List of comments
     private List<Comment> Comments { get; set;}
+
+    // constructor
     public Video(string title, string author, int lengthInSeconds)
     {
         Title = title;
@@ -29,14 +35,21 @@ public class Comment
         LengthInSeconds = lengthInSeconds;
         Comments = new List<Comment>();
     }
+
+    // Method to add a comment
     public void AddComment(Comment comment)
     {
         Comments.Add(comment);
     }
+
+    // Method to get the number of comments
     public int GetNumberOfComments()
     {
         return Comments.Count;
     }
+
+    // Method to display all comments
+
     public void DisplayComments()
     {
         foreach (var comment in Comments)
