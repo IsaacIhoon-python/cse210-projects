3b88fbac4ac894691269d892714d0a8fb15215cb
IsaacIhoon-python <ihoonisaac@gmail.com>
2026-02-05 04:18:52 +0100

    All classes included

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment1 = new Assignment("Isaac Ihoon", "Inheritance");
+        Console.WriteLine(assignment1.GetSummary());
+
+        MathAssignment assignment2 = new MathAssignment(
+            "Isaac Ihoon",
+            "Fractions",
+            "7.3",
+            "8-19"
+        );
+
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetHomeworkList());
+
+        WritingAssignment assignment3 = new WritingAssignment(
+            "Isaac Ihoon",
+            "World History",
+            "The Causes of World War II"
+        );
+
+        Console.WriteLine(assignment3.GetSummary());
+        Console.WriteLine(assignment3.GetWritingInformation());
+
     }
+    
 }
