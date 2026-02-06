019aeb554d7b3e60dc4fa94f6b0c03db73bcfea0
IsaacIhoon-python <ihoonisaac@gmail.com>
2026-02-06 13:17:51 +0100

    Did some modification added  a class for Video, and added a new screen short

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -1,47 +1,41 @@
+using System;
+using System.Collections.Generic;
+
 class Program
 {
     static void Main(string[] args)
     {
-        // Created videos
-
+        // Create videos
         Video video1 = new Video("How to Bake Bread", "Isaac", 540);
         Video video2 = new Video("Understanding C# OOP", "Bright", 620);
         Video video3 = new Video("Top 10 Hacking Tips", "Fletcher", 480);
 
-        
-        // Add comments to video1
+        // Add comments to video1 (3 comments)
         video1.AddComment(new Comment("Stanley", "This was very helpful!"));
         video1.AddComment(new Comment("John", "Loved the step-by-step explanation."));
-        video1.AddComment(new Comment("Marry", "Can you do a video on how to Package the Bread? "));
+        video1.AddComment(new Comment("Mary", "Can you do a video on packaging bread?"));
 
-        // Add comments to video2
+        // Add comments to video2 (3 comments)
         video2.AddComment(new Comment("Benjamin", "This is so insightful!"));
-        video2.AddComment(new Comment("Prince", "I Love the detailed explanation."));
-        video2.AddComment(new Comment("Lucky", "Can you also make a video on Python? "));
+        video2.AddComment(new Comment("Prince", "I love the detailed explanation."));
+        video2.AddComment(new Comment("Lucky", "Can you also make a video on Python?"));
 
-        // Add comment to video3
-        video3.AddComment(new Comment("Solomon", "Wao! I never thought this was possible...."));
-        video3.AddComment(new Comment("Francis", "I just want to be sure if its's ethical ."));
-        video3.AddComment(new Comment("Victoria", "Can you do a follow-up video on Penetration testing? "));
+        // Add comments to video3 (3 comments)
+        video3.AddComment(new Comment("Solomon", "Wow! I never thought this was possible."));
+        video3.AddComment(new Comment("Francis", "Is this ethical?"));
+        video3.AddComment(new Comment("Victoria", "Can you do a follow-up on penetration testing?"));
 
         // Store videos in a list
-        List<Video> videos = new List<Video> { video1, video2, video3};
+        List<Video> videos = new List<Video> { video1, video2, video3 };
 
-        // Display information for each video
-        foreach (var video in videos)
+        // Display each video
+        foreach (Video video in videos)
         {
-            Console.WriteLine($"Title: {video.Title}");
-            Console.WriteLine($"Author: {video.Author}");
-            Console.WriteLine($"Length: {video.LengthInSeconds} seconds");
-            Console.WriteLine($"Number of Comments: {video.GetNumberOfComments()}");
-            Console.WriteLine("Comments:");
-            video.DisplayComments();
+            video.DisplayVideoInfo();
             Console.WriteLine(new string('_', 40));
         }
 
-        // Pause console
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
-
     }
 }
--- week04/YouTubeVideos/comment.cs
+++ week04/YouTubeVideos/comment.cs
@@ -1,69 +1,21 @@
-using System;
-using System.Collections.Generic;
-
-// Class representing a Comment
 public class Comment
 {
-    // Properties
-    public string CommenterName { get; set; }
-    public string Text { get; set;}
+    private string _commenterName;
+    private string _text;
 
-    // Constructor
     public Comment(string commenterName, string text)
     {
-        CommenterName = commenterName;
-        Text = text;
+        _commenterName = commenterName;
+        _text = text;
+    }
+
+    public string GetCommenterName()
+    {
+        return _commenterName;
+    }
+
+    public string GetText()
+    {
+        return _text;
     }
 }
-    // Class representing a Video
-    public class Video
-    {
-        // Properties
-        public string Title { get; set;}
-        public string Author { get; set;}
-        public int LengthInSeconds { get; set;}
-
-    
-        // List of comments
-    private List<Comment> Comments { get; set;}
-
-    // constructor
-    public Video(string title, string author, int lengthInSeconds)
-    {
-        Title = title;
-        Author = author;
-        LengthInSeconds = lengthInSeconds;
-        Comments = new List<Comment>();
-    }
-
-    // Method to add a comment
-    public void AddComment(Comment comment)
-    {
-        Comments.Add(comment);
-    }
-
-    // Method to get the number of comments
-    public int GetNumberOfComments()
-    {
-        return Comments.Count;
-    }
-
-    // Method to display all comments
-
-    public void DisplayComments()
-    {
-        foreach (var comment in Comments)
-        {
-            Console.WriteLine($"- {comment.CommenterName}: {comment.Text}");
-        }
-    }
-}
-       
-
-
-
-
-
-
-
- 
